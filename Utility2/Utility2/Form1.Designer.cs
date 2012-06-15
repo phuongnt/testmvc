0e3ea4316717e29d02fe60205a83b1acf5e40716
Phuong Nguyen <trangphuong618@gmail.com>
2012-06-15 15:53:54 +0700

    utility

---
+++ Utility2/Utility2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Utility2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btGenerate = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.RichTextBox();
+            this.txtKeyDown = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtURLPost = new System.Windows.Forms.TextBox();
+            this.lbCount = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btGenerate
+            // 
+            this.btGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btGenerate.Location = new System.Drawing.Point(377, 286);
+            this.btGenerate.Name = "btGenerate";
+            this.btGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btGenerate.TabIndex = 0;
+            this.btGenerate.Text = "Generate";
+            this.btGenerate.UseVisualStyleBackColor = true;
+            this.btGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 286);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(348, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.Location = new System.Drawing.Point(12, 332);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(684, 276);
+            this.txtContent.TabIndex = 3;
+            this.txtContent.Text = "";
+            // 
+            // txtKeyDown
+            // 
+            this.txtKeyDown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKeyDown.Location = new System.Drawing.Point(12, 4);
+            this.txtKeyDown.Name = "txtKeyDown";
+            this.txtKeyDown.Size = new System.Drawing.Size(684, 163);
+            this.txtKeyDown.TabIndex = 4;
+            this.txtKeyDown.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(314, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(324, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(405, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtURLPost
+            // 
+            this.txtURLPost.Location = new System.Drawing.Point(12, 230);
+            this.txtURLPost.Name = "txtURLPost";
+            this.txtURLPost.Size = new System.Drawing.Size(306, 20);
+            this.txtURLPost.TabIndex = 8;
+            this.txtURLPost.Text = "http://www.hnammobile.com/api/vote?item_id=79";
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Location = new System.Drawing.Point(186, 257);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(13, 13);
+            this.lbCount.TabIndex = 9;
+            this.lbCount.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(570, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 620);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lbCount);
+            this.Controls.Add(this.txtURLPost);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtKeyDown);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btGenerate;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.RichTextBox txtContent;
+        private System.Windows.Forms.RichTextBox txtKeyDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtURLPost;
+        private System.Windows.Forms.Label lbCount;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Utility2/Utility2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.IO;
+using System.Net;
+
+namespace Utility2
+{
+    public partial class Form1 : Form
+    {
+        KeyboardHook _keyboardHook;
+        string KeyDownValue = "";
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+          
+
+            ListBox listBox1 = new ListBox();
+            listBox1.Location = new Point(10, 10);
+            listBox1.Size = new Size(200, 200);
+
+            //this.Controls.Add(listBox1);
+
+            _keyboardHook = new KeyboardHook();
+            _keyboardHook.Install();
+
+            _keyboardHook.KeyDown += (sender, e) =>
+            {
+                //KeyDownValue += e.KeyCode;
+                //txtKeyDown.Text = KeyDownValue;
+                //SaveFile(KeyDownValue);
+                //listBox1.Items.Add("KeyDown: " + e.KeyCode);
+
+                //listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            };
+
+            _keyboardHook.KeyUp += (sender, e) =>
+            {
+                KeyDownValue += e.KeyCode;
+                txtKeyDown.Text = KeyDownValue;
+                SaveFile(KeyDownValue);
+                //listBox1.Items.Add("KeyUp: " + e.KeyCode);
+
+                //listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var httpDownload = new HttpSession();
+            var sUrl = "";
+            var sPost = "";
+            /////
+            sUrl = txtUrl.Text;
+            sPost = "";
+            CookieCollection Cookie = new CookieCollection();
+            Cookie.
+            var sContent = httpDownload.PostMethodDownload(sUrl, sPost, false, false, false, false);
+
+            txtContent.Text = sContent;
+
+        }
+
+        public void SaveFile(string Content)
+        {
+            //FileInfo ofile = new FileInfo(Application.ExecutablePath + "\\output.txt");
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\output.txt", false);
+            
+            sw.Write(Content);
+            sw.Close();
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            txtKeyDown.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var httpDownload = new HttpSession();
+            var sUrl = txtURLPost.Text;// "http://www.hnammobile.com/api/vote?item_id=79";
+            var sPost = "";
+            /////
+            //sUrl = txtUrl.Text;
+            sPost = "";
+            httpDownload.PostMethodDownload(sUrl, sPost, true, false, false, false);
+            count++;
+            lbCount.Text = count.ToString();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
