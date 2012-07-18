b11bbc86c0e77ede1ec4ed285f6bf5f592d0002a
Phuong Nguyen <trangphuong618@gmail.com>
2012-07-18 17:23:05 +0700

    2

---
+++ cooking/cooking/Controllers/koController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cooking.Controllers
+{
+    public class koController : Controller
+    {
+        //
+        // GET: /ko/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleList()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleGrid()
+        {
+            return View();
+        }
+
+    }
+}
